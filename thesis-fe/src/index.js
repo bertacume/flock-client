a092613eef7ebd2bb02197c12264e40ebf6a3dca
ArtyBMPS <art_bmps@hotmail.com>
2018-11-28 10:46:17 +0100

    added auth workflow on apollo

--- thesis-fe/src/index.js
+++ thesis-fe/src/index.js
@@ -3,12 +3,32 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import ApolloClient from "apollo-boost";
+import ApolloClient from "apollo-client";
 import { ApolloProvider } from "react-apollo";
 import { serverURL } from './helpers/constants';
+import { createHttpLink } from 'apollo-link-http';
+import { setContext } from 'apollo-link-context';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+
+const httpLink = createHttpLink({
+  uri: serverURL
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('token');
+  console.log('aaaaaaaa');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : "",
+    }
+  }
+});
 
 const client = new ApolloClient({
-  uri: serverURL
+  uri: serverURL,
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache()
 });
 
 
