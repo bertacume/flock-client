5d5fbf421a08ea78eb85bc02812991edc5c55120
Berta Cume <bertacume@gmail.com>
2018-12-01 14:29:13 +0100

    fix: delete userID field from createTrip mutation

--- thesis-fe/src/components/apollo/mutations/create_trip.js
+++ thesis-fe/src/components/apollo/mutations/create_trip.js
@@ -1,7 +1,7 @@
 import gql from "graphql-tag";
 
 const CREATE_TRIP = gql`mutation createTrip($trip: TripInput!) {
-  createTrip(trip: $trip, userID: "5bffbe8ff59a127f1afadf18") {
+  createTrip(trip: $trip) {
     id
   }
 }
