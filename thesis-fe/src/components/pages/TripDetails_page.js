9904263ed1246ab1cf803d22118de17e1ea05c85
ArtyBMPS <art_bmps@hotmail.com>
2018-12-05 19:28:33 +0100

    fixed trip details general query

--- thesis-fe/src/components/pages/TripDetails_page.js
+++ thesis-fe/src/components/pages/TripDetails_page.js
@@ -23,77 +23,59 @@ class TripDetails_page extends Component {
   }
   render() {
     const TripDetailsApollo = () => (
-      <Query
-        query={GET_TRIP_DETAILS}
-        errorPolicy="all"
-        variables={{ tripID: this.props.match.params.id }}
-      >
-        {({ subscribeToMore, loading, error, data }) => {
-          if (loading) return <h1>Loading</h1>;
-          if (error) console.log(error);
-          return (
-            (data.trip) ?
-                  <Container>
-                <Navbar
-                  pathLeft={`/mytrips`}
-                  pathRight={`/tripdetails/${data.trip.id}/chat/general`}
-                  title={data.trip.name}
-                  history={this.props.history}
-                  iconRight={chat}
-                />
-                <GeneralInfoDashboard history={this.props.history} match={this.props.match} info={data.trip} redirectParent={this.redirectParent()} sub={
-                  () => subscribeToMore({
-                    document: GET_TRIP_DETAILS_SUB,
-                    variables: { tripID: this.props.match.params.id },
-                    updateQuery: (prev, { subscriptionData }) => {
-                      if (!subscriptionData.data || !subscriptionData.data.tripInfoChanged) return prev;
-                      let type;
-                      if (subscriptionData.data.tripInfoChanged.participants && subscriptionData.data.tripInfoChanged.participants.length !== prev.trip.participants.length) type = 'participants';
-                      else if (subscriptionData.data.tripInfoChanged.destination.chosenSuggestion) {
-                        type = 'destination'
+      <Container>
+        <Query
+          query={GET_TRIP_DETAILS}
+          errorPolicy="all"
+          variables={{ tripID: this.props.match.params.id }}
+        >
+          {({ subscribeToMore, loading, error, data }) => {
+            if (loading) return <h1>Loading</h1>;
+            if (error) console.log(error);
+            return (
+              (data.trip) ?
+                <div>
+                  <Navbar
+                    pathLeft={`/mytrips`}
+                    pathRight={`/tripdetails/${data.trip.id}/chat/general`}
+                    title={data.trip.name}
+                    history={this.props.history}
+                    iconRight={chat}
+                  />
+                  <GeneralInfoDashboard history={this.props.history} match={this.props.match} info={data.trip} redirectParent={this.redirectParent()} sub={
+                    () => subscribeToMore({
+                      document: GET_TRIP_DETAILS_SUB,
+                      variables: { tripID: this.props.match.params.id },
+                      updateQuery: (prev, { subscriptionData }) => {
+                        return prev;
                       }
-                      else if (subscriptionData.data.tripInfoChanged.budget.chosenSuggestion) {
-                        type = 'budget'
-                      }
-
-                      if (type && subscriptionData.data.tripInfoChanged[type].chosenSuggestion && subscriptionData.data.tripInfoChanged[type].chosenSuggestion.id && subscriptionData.data.tripInfoChanged.id === this.state.tripID) {
-                        const newObject = {
-                          ...prev,
-                          trip: {
-                            ...prev.trip,
-                            [type]: subscriptionData.data.tripInfoChanged[type]
-                          }
-                        }
-                        return newObject;
-                      }
-                      else return prev;
                     }
+                    )
                   }
-                  )
-                }
-                />
-                </Container>
-              :
-              <h1>Sorry, trip not found</h1>
-          )
-        }
-        }
-      </Query>
-    );
-    return (
+                  />
+                </div>
+                :
+                <h1>Sorry, trip not found</h1>
+              )
+            }
+          }
+        </Query>
+      </Container>
+        );
+        return (
       <TripDetailsApollo />
-    );
-  }
-}
+        );
+      }
+    }
 
-export default TripDetails_page
+    export default TripDetails_page
 
-const Container = styled('div')`
-  // box-sizing: border-box;
-  width: 100%;
-  height: 100%;
-  display: flex;
-  flex-direction: column;
-  justify-content: flex-start;
-  align-items: center;
-`
+    const Container = styled('div')`
+      // box-sizing: border-box;
+      width: 100%;
+      height: 100%;
+      display: flex;
+      flex-direction: column;
+      justify-content: flex-start;
+      align-items: center;
+    `
